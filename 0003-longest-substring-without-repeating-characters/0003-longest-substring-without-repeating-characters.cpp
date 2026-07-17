baa133b9b3290f8602a8f2d006cff0e65f0acad0
Yash tyagi <yashxtyagi06@gmail.com>
2026-07-17 13:21:22 +0530

    Time: 3 ms (80.63%), Space: 11.8 MB (55.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,10 +8,15 @@ public:
 
 
         for(int r = 0; r < n; r ++){
-            if(mp.find(s[r]) != mp.end())
+            if(mp.find(s[r]) != mp.end()) // duplicate mila?
+                // left ko duplicate ke baad le jao,
+                // leking kabhi peeche nhi lana
                 left= max (left, mp[s[r]]+ 1);
+
+                // current index ka latest index update kiya
             mp[s[r]]= r;
 
+                // window ka size nikala and ans update kiya
             ans= max(ans, r-left + 1);
         }
 
