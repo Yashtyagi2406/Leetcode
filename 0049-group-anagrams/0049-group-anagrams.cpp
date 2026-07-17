78d43b84ea98108af24a278b416c76a1a86d42b0
Yash tyagi <yashxtyagi06@gmail.com>
2026-07-18 00:24:54 +0530

    Time: 75 ms (7.18%), Space: 37.5 MB (5.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,15 +4,21 @@ public:
         unordered_map<string, vector<string>> mp;
 
         for( string s: strs){
-            string temp = s;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s);
+            vector<int> freq (26,0);
+
+            for(char c : s){
+                freq[c - 'a']++;
+            }
+            string key ="";
+            for(int i =0; i< 26; i++){
+                key += to_string(freq[i]) + "#";
+            }
+            mp[key].push_back(s);
         }
         vector<vector<string>> ans;
-
-        for(auto it: mp){
+        for(auto it :mp){
             ans.push_back(it.second);
         }
-        return ans;
+         return ans;
     }
 };
