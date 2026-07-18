27f3cb40006b50271b4910c6db501a32d8c4d600
Yash tyagi <yashxtyagi06@gmail.com>
2026-07-18 13:47:08 +0530

    Time: 4 ms (33.79%), Space: 17.8 MB (97.53%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<pair<int,int>> freq;
+        vector<int> ans;
+
+       sort(nums.begin(), nums.end());
+
+       for(int i= 0; i< nums.size(); i++){
+            int count = 1;
+            while( i+ 1< nums.size() && nums[i] == nums[i+ 1]){
+                count ++;
+                i++;
+            }
+             freq.push_back({nums[i], count});
+       }
+             sort(freq.begin(), freq.end(),
+                    [](pair<int,int> a, pair<int, int> b){
+                        return a.second> b.second;
+                    }
+                );
+            for(int i= 0; i< k ; i++)
+                ans.push_back(freq[i].first);
+            
+        return ans;
+    }
+};
