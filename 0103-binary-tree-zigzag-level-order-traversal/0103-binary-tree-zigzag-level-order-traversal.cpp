e24e73a737822b286459e87b2e97fa181d7fb864
Yash tyagi <yashxtyagi06@gmail.com>
2026-07-25 11:17:07 +0530

    Time: 0 ms (100%), Space: 15.1 MB (48.69%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -15,21 +15,21 @@ public:
         while(!q.empty()){
 
             int size = q.size();
-            vector<int> level;
+            vector<int> level(size);
 
-            while(size--){
+            for(int i= 0; i< size;  i++){
                 TreeNode* node = q.front();
                 q.pop();
-                level.push_back(node->val);
 
+                int index = leftToRight ? i: size -1-i;
+                level[index] = node -> val;
                 if(node->left)
                     q.push(node->left);
                 if(node->right)
                     q.push(node->right);
             }
 
-            if(!leftToRight)
-                reverse(level.begin(), level.end());
+        
 
             ans.push_back(level);
 
