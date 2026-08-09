d2333924f8c397b2113a558aceb5a4e8bb146daf
Yash tyagi <yashxtyagi06@gmail.com>
2026-08-09 13:47:28 +0530

    Time: 4 ms (86.19%), Space: 44.9 MB (48.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,8 +5,7 @@ public:
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int i = 0;
-        int j = 0;
+        int i =0, j= 0;
 
         while (i < g.size() && j < s.size()) {
             if (s[j] >= g[i]) {
