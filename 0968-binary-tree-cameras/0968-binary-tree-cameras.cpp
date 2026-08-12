fbdf4fe12cf3b7af27e127555879dbbbfa246ebd
Yash tyagi <yashxtyagi06@gmail.com>
2026-08-12 22:51:55 +0530

    Time: 0 ms (100%), Space: 21.4 MB (35.5%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -4,31 +4,36 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode() : val(NEED), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int camera =0;
+    int camera =NEED;
+    enum State{
+        NEED = 0,
+        CAMERA = 1,
+        COVERED = 2
+    };
     int dfs(TreeNode* root){
-        if(root == NULL) return 2;
+        if(root == NULL) return COVERED;
 
         int left = dfs(root->left);
         int right = dfs(root->right);
 
-        if(left == 0 || right ==0){
+        if(left == NEED || right ==NEED){
             camera ++;
-            return 1;
+            return CAMERA;
         }
-        if(left == 1 || right == 1) return 2;
+        if(left == CAMERA || right == CAMERA) return COVERED;
 
-        return 0;
+        return NEED;
     }
 
     int minCameraCover(TreeNode* root) {
-        if(dfs(root)== 0)
+        if(dfs(root)== NEED)
             camera ++;
 
         return camera;
