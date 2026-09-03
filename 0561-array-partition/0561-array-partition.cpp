dc6c679bfebcd215aaefc492ac9a023be35ff268
Yash tyagi <yashxtyagi06@gmail.com>
2026-09-03 23:20:20 +0530

    Time: 10 ms (59.41%), Space: 32.1 MB (21.33%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
 
         int sum = 0;
-        for(int i= 0; i< nums.size() ; i= i+ 2){
+        for(int i= 0; i< nums.size() ; i += 2){
 
             sum += nums[i];
         }
